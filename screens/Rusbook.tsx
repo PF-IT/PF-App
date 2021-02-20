38057a6d40bd98357886c76b74d599b4af8480ce
starostka <benjamin.starostka@gmail.com>
2021-02-20 09:57:27 +0100

    Update Rusbook.tsx

--- screens/Rusbook.tsx
+++ screens/Rusbook.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import useSWR from 'swr';
 import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, Text, TopNavigation, BottomNavigation, Icon } from '@ui-kitten/components';
+import { Button, Divider, Layout, Text, TopNavigation, BottomNavigation, Icon, ListItem, List } from '@ui-kitten/components';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { StackNavigationState } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,6 +9,7 @@ import RusWelcome from './rusbook/RusWelcomeScreen';
 import RusPolytekniskForening from './rusbook/RusPolytekniskForening';
 import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { NavSelectCard } from '../components/NavSelectCard';
+import { graphql_fetcher } from '../utils/api';
 
 // TODO: MAY MOVE THIS TO EALIER STAGE OF PROGRAM LIKE types.tsx or App.tsx
 type RusbookStackParamList = {
@@ -24,28 +26,42 @@ type RusbookProps = {
     navigation: any;
 };
 
-export const RusbookScreen = ({ navigation }: any) => (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ margin: 10 }} category='h1'>Select chapter</Text>
-        <Divider />
-        <ScrollView contentContainerStyle={styles.contentContainer} >
-            {/* CONVERT THESE INTO BEAUTIFUL CARDS */}
+export const RusbookScreen = ({ navigation }: any) => {
+    //Data should be a list of RusbookChapter navigation options
+    const { data, error } = useSWR('/graphql/rusbooks', graphql_fetcher);
+
+    //We will render a list of these items once we've retrieved them from the DB. Until then we'll show a spinner and on error an error message.
+
+    //This function component will take an item instance and create a listItem based on the instance information, index is used to show the chapters in correct order
+    const renderRusChapter = ({dataItem, index }:any) => (
+        <ListItem
+            title={`${dataItem.title}`}
+            description={`${dataItem.description}`}
+            onPress={() => {navigation.navigate(dataItem.route)}}
+        />
+    );
+
+    return (
+        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ margin: 10 }} category='h1'>Select chapter</Text>
+            <Divider />
+            {/* <List
+                style={styles.contentContainer}
+                data={data} // swr will update the data
+                ItemSeparatorComponent={Divider}
+                renderItem={renderRusChapter} // component function will render data appropriately once retrieved
+            /> */}
+            <ScrollView contentContainerStyle={styles.contentContainer} >
             <NavSelectCard
                 navigation={navigation}
                 route='Welcome'
                 title='Welcome'
                 icon_src={require('~/assets/images/favicon.png')}
                 description="Welcome to DTU let us tell you about opportunities available through DTU and PF." />
-            {/* <Button style={styles.rusbookButton} onPress={() => navigation.navigate('Welcome')}>Welcome</Button>
-            <Button style={styles.rusbookButton} onPress={() => navigation.navigate('PolytekniskForening')}>Polyteknisk Forening</Button>
-            <Button style={styles.rusbookButton} disabled={true} onPress={() => navigation.navigate('Education')}>Education</Button>
-            <Button style={styles.rusbookButton} disabled={true} onPress={() => navigation.navigate('StudentLife')}>Student Life</Button>
-            <Button style={styles.rusbookButton} disabled={true} onPress={() => navigation.navigate('DTU')}>DTU</Button>
-            <Button style={styles.rusbookButton} disabled={true} onPress={() => navigation.navigate('Dorms')}>Dorms</Button>
-            <Button style={styles.rusbookButton} disabled={true} onPress={() => navigation.navigate('More')}>More</Button> */}
         </ScrollView>
-    </Layout>
-);
+        </Layout>
+    )
+};
 
 const styles = StyleSheet.create({
     contentContainer: {
