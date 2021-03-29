6d4b1e7a05966e786ec62ed27cc1eca1eca1e2b1
Sam Rahbar <sam@rahbar.dk>
2021-03-29 15:56:05 +0200

    RusbookChapter styling
    Error if no content

--- components/NavSelectCard.tsx
+++ components/NavSelectCard.tsx
@@ -23,28 +23,27 @@ import {
 } from "react-native";
 import { FlexStyleProps, RenderProp } from "@ui-kitten/components/devsupport";
 
-
 type NavSelectCardProps = {
-  navigation: StackNavigationProp<any, string>
-  chapter_id: string,
-  title: string,
-  icon_src: number | ImageURISource,
-  description: string
+  navigation: StackNavigationProp<any, string>;
+  chapter_id: string;
+  title: string;
+  icon_src: number | ImageURISource;
+  description: string;
 };
 
-
 export const NavSelectCard = (props: NavSelectCardProps) => {
+  const styles = useStyleSheet(themedStyles);
 
   const Header = (props: any) => (
-    <View {...props} style={[props.style, themedStyles.headerContainer]}>
-      <Text category="h4" style={(props.style, themedStyles.headerText)}>
+    <View {...props} style={[props.style, styles.headerContainer]}>
+      <Text category="h4" style={(props.style, styles.headerText)}>
         Header
       </Text>
     </View>
   );
 
   const Footer = (props: any) => (
-    <View {...props} style={[props.style, themedStyles.footerContainer]}>
+    <View {...props} style={[props.style, styles.footerContainer]}>
       <Text category="h4" style={{ color: "white" }}>
         {props.title}
       </Text>
@@ -52,74 +51,76 @@ export const NavSelectCard = (props: NavSelectCardProps) => {
     </View>
   );
 
-    return (
-      <Card
-        onPress={() => props.navigation.navigate('RusbookChapter', { chapter_id: props.chapter_id })}
-        style={themedStyles.card}
-        header={() => <Header title={props.title} />}
+  return (
+    <Card
+      onPress={() =>
+        props.navigation.navigate("RusbookChapter", {
+          chapter_id: props.chapter_id,
+        })
+      }
+      style={styles.card}
+      header={() => <Header title={props.title} />}
       // footer={() => <Footer title={props.title}/>}
-      >
-        <View style={themedStyles.content}>
-          <Avatar shape="square" size="large" source={props.icon_src} />
-          <Text category="p2" style={themedStyles.description}>
-            {props.description}
-          </Text>
-        </View>
-      </Card>
-    );
-  };
+    >
+      <View style={styles.content}>
+        <Avatar shape="square" size="large" source={props.icon_src} />
+        <Text category="p2" style={styles.description}>
+          {props.description}
+        </Text>
+      </View>
+    </Card>
+  );
+};
 
-  const themedStyles = StyleService.create({
-    headerText: {
-      color: "nav-header-font-color",
+const themedStyles = StyleService.create({
+  headerText: {
+    color: "nav-header-font-color",
+  },
+  headerContainer: {
+    backgroundColor: "color-primary-500",
+    padding: 5,
+    paddingLeft: 10,
+    borderRadius: 4,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    headerContainer: {
-      backgroundColor: "color-primary-500",
-      padding: 5,
-      paddingLeft: 10,
-      borderRadius: 4,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 4,
-      },
-      shadowOpacity: 0.32,
-      shadowRadius: 5.46,
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
 
-      elevation: 5,
+    elevation: 5,
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: -10,
+  },
+  footerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "blue", // TODO: get colors from theme
+  },
+  card: {
+    marginVertical: 4,
+    borderRadius: 4,
+    borderWidth: 0,
+    width: 300,
+    shadowColor: "#000",
+    backgroundColor: "nav-card-background-color",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    content: {
-      flexDirection: "row",
-      alignItems: "center",
-      margin: -10,
-    },
-    footerContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      backgroundColor: "blue", // TODO: get colors from theme
-    },
-    card: {
-      flex: 1,
-      justifyContent: "flex-start",
-      marginVertical: 4,
-      borderRadius: 4,
-      borderWidth: 0,
-      width: "90%",
-      shadowColor: "#000",
-      backgroundColor: "nav-card-background-color",
-      shadowOffset: {
-        width: 0,
-        height: 4,
-      },
-      shadowOpacity: 0.32,
-      shadowRadius: 5.46,
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
 
-      elevation: 3,
-    },
-    description: {
-      marginLeft: 20,
-      marginRight: 45,
-      fontSize: 12,
-      fontFamily: "Roboto",
-    },
-  });
+    elevation: 3,
+  },
+  description: {
+    marginLeft: 20,
+    marginRight: 45,
+    fontSize: 12,
+    fontFamily: "Roboto",
+  },
+});
--- screens/rusbook/RusbookChapter.tsx
+++ screens/rusbook/RusbookChapter.tsx
@@ -1,22 +1,36 @@
-import * as React from 'react';
-import { Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Divider, Icon, Layout, List, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
-import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { useAuth } from '../../utils/Auth';
+import * as React from "react";
+import { Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Divider,
+  Icon,
+  Layout,
+  List,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+} from "@ui-kitten/components";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  PureComponent,
+} from "react-native";
+import { useAuth } from "../../utils/Auth";
 import gql from "graphql-tag";
-import useSWR from 'swr';
-import { baseurl, graphqlFetchWithToken } from '../../utils/api';
-import Markdown from 'react-native-markdown-renderer';
+import useSWR from "swr";
+import { baseurl, graphqlFetchWithToken } from "../../utils/api";
+import Markdown from "react-native-markdown-renderer";
 
 // API Fetch function request chapter content
 // TODO: should be put into a common API collection
-function rusbookChapterContent(id:any) {
-    const { status, authToken, basicAuthToken } = useAuth();
-    
-    // TODO: ensure we wait for a token before making a request!
-    
-    const query = gql`query {
+function rusbookChapterContent(id: any) {
+  const { status, authToken, basicAuthToken } = useAuth();
+
+  // TODO: ensure we wait for a token before making a request!
+
+  const query = gql`query {
             rusbookChapter(id: ${id}){
                 Title,
                 primary_color,
@@ -40,87 +54,134 @@ function rusbookChapterContent(id:any) {
     }
     `;
 
-    const { data, error } = useSWR(
-        [query, basicAuthToken],
-        graphqlFetchWithToken
-    )
+  const { data, error } = useSWR(
+    [query, basicAuthToken],
+    graphqlFetchWithToken
+  );
 
-    console.log(data);
+  console.log(data);
 
-    return {
-        chapterContent: data ? data.rusbookChapter : undefined,
-        isLoading: !error && !data,
-        isError: error,
-      };
+  return {
+    chapterContent: data ? data.rusbookChapter : undefined,
+    isLoading: !error && !data,
+    isError: error,
+  };
 }
 
-const RenderContent = (chapterZone:any) => {
-    console.log("The chapterZone");
-    
-    console.log(JSON.stringify(chapterZone));
-    
-    switch (chapterZone.item.__typename) {
-        case "ComponentDefaultRichText":
-            return (
-                <Markdown>{chapterZone.item.richText}</Markdown>
-                // <Text style={styles.richText}>{chapterZone.item.richText}</Text>
-            );
-        case "ComponentDefaultImage":
-            return (
-                <Image style={styles.image} source={{uri: baseurl + chapterZone.item.image.url}}/>
-            );
-        default:
-            return (<Text category="p1">NOT A VALID __TYPENAME</Text>)
-    }
-}
+const RenderContent = (chapterZone: any) => {
+  console.log("The chapterZone");
 
-export default function RusbookChapter({route, navigation}:any) {
-    const { chapterContent, isLoading, isError } = rusbookChapterContent(route.params.chapter_id);
-    React.useLayoutEffect(() => {
-        navigation.setOptions({title: chapterContent.Title});
-    }, [navigation, chapterContent.Title])
-    console.log("params: " + JSON.stringify(route.params))
+  console.log(JSON.stringify(chapterZone));
 
-    // TODO: add nice error and loading components
-    if (isError) return <Text category="p2">Error</Text>;
-    if (isLoading) return <Text category="p2">Loading...</Text>;
+  switch (chapterZone.item.__typename) {
+    case "ComponentDefaultRichText":
+      return (
+        <Markdown style={mdstyles}>{chapterZone.item.richText}</Markdown>
+        // <Text style={styles.richText}>{chapterZone.item.richText}</Text>
+      );
+    case "ComponentDefaultImage":
+      return (
+        <Image
+          style={styles.image}
+          source={{ uri: baseurl + chapterZone.item.image.url }}
+        />
+      );
+    default:
+      return <Text category="p1">NOT A VALID __TYPENAME</Text>;
+  }
+};
 
-    return (
-        // TODO: add content.cover as a hero
-        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text category='h1'>{chapterContent.Title}</Text>
-            <Text category='p1'>{chapterContent.Description}</Text>
-            <List
-            style={styles.listContainer}
-            contentContainerStyle={styles.contentContainer}
-            data={chapterContent.rusbookChapterZone}
-            renderItem={RenderContent}
-            // extraData={navigation}
-            />
-        </Layout>
-        
-    );
+export default function RusbookChapter({ route, navigation }: any) {
+  const { chapterContent, isLoading, isError } = rusbookChapterContent(
+    route.params.chapter_id
+  );
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ title: chapterContent.Title });
+  }, [navigation, chapterContent.Title]);
+  console.log("params: " + JSON.stringify(route.params));
+
+  // TODO: add nice error and loading components
+  if (isError) return <Text category="p2">Error</Text>;
+  if (isLoading) return <Text category="p2">Loading...</Text>;
+
+  return (
+    // TODO: add content.cover as a hero
+    <Layout style={styles.layout}>
+      {/* Removed: */}
+      {/* <Layout style={styles.header}>
+        // Should be in navigation top:   
+        <Text category="h1">{chapterContent.Title}</Text>
+        // Should only be seen in the rusbook navigation:
+        <Text category="p1">{chapterContent.Description}</Text>
+      </Layout> */}
+      <List
+        style={styles.listContainer}
+        contentContainerStyle={styles.contentContainer}
+        data={chapterContent.rusbookChapterZone}
+        renderItem={RenderContent}
+        // extraData={navigation}
+      />
+    </Layout>
+  );
 }
 
 const styles = StyleSheet.create({
-    listContainer: {
-      // maxHeight: 320,
-    },
-    contentContainer: {
-      // flex: 1,
-      alignItems: "center",
-    },
-    rusbookButton: {
-      margin: 10,
-      width: 200,
-    },
-    image: {
-        padding: 10,
-        width: 200,
-        height: 200,
-    },
-    richText: {
+  layout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  header: {
+    backgroundColor: "white",
+    width: "100%",
+    paddingLeft: "5%",
+    borderBottomWidth: 10,
+  },
+  listContainer: {
+    width: "100%",
+  },
+  contentContainer: {
+    alignItems: "center",
+    backgroundColor: "white",
+    width: "100%",
+  },
+  rusbookButton: {
+    margin: 10,
+    width: 200,
+  },
+  image: {
+    margin: 10,
+    width: 400,
+    height: 200,
+  },
+  richText: {},
+});
 
-    }
-  });
-  
+const mdstyles = StyleSheet.create({
+  text: { width: "90%", margin: 0, padding: 0 },
+  strong: { fontWeight: "bold" },
+  em: { fontStyle: "italic" },
+  link: { fontWeight: "bold", color: "blue" },
+
+  heading: {},
+  heading1: {
+    fontSize: 32,
+    fontWeight: "normal",
+  },
+  heading2: {
+    fontSize: 24,
+  },
+  heading3: {
+    fontSize: 18,
+  },
+  heading4: {
+    fontSize: 16,
+  },
+  heading5: {
+    fontSize: 13,
+  },
+  heading6: {
+    fontSize: 11,
+  },
+});
