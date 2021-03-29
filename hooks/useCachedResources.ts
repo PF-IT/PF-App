f77abdc34e5b5e1e54a51789ad024619b7846961
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2021-03-29 12:08:30 +0200

    merged

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -17,6 +17,7 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'Roboto': require('../assets/fonts/Roboto/Roboto-Regular.ttf')
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
